d850bd22b6d70c6cad3613d564043e2a33357ffe
Oswaldo Gomez <oswaldogomez@devtohack.com>
2018-07-30 05:00:52 -0300

    Fixed reducer and action players constant.

---
+++ src/footballPlayers/testConstants/players.constants.js
@@ -0,0 +1,14 @@
+export const players = [
+  {
+    name: 'testName1',
+    position: 'testPosition1',
+    nationality: 'testNationality1',
+    dateOfBirth: '2000-01-01'
+  },
+  {
+    name: 'testName2',
+    position: 'testPosition2',
+    nationality: 'testNationality2',
+    dateOfBirth: '2000-01-01'
+  }
+];;
