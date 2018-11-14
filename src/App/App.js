8fff37a305a60e38e6231b328885c1e999fc58e9
Oswaldo Gomez <oswaldogomezsl@gmail.com>
2018-11-14 19:04:44 -0300

    ToDo: App.test.js refactor

--- src/App/App.js
+++ src/App/App.js
@@ -1,17 +1,13 @@
-import React, { Component } from 'react';
+import React from 'react';
 import '../css/tailwind.css';
 import footballPlayers from '../footballPlayers';
 
 const { FootballPlayers } = footballPlayers.components;
 
-class App extends Component {
-  render() {
-    return (
-      <div className="container mx-auto px-2 md:px-0 py-4">
-        <FootballPlayers />
-      </div>
-    );
-  }
-}
+const App = () => (
+  <div className="container mx-auto px-2 md:px-0 py-4">
+    <FootballPlayers />
+  </div>
+)
 
 export default App;
--- src/App/App.test.js
+++ src/App/App.test.js
@@ -2,6 +2,8 @@ import React from 'react';
 import { shallow } from 'enzyme';
 import App from './App';
 
+// ToDo: refactor
+
 it('renders without crashing', () => {
   shallow(<App />);
 });
